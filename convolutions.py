aeae0a4c188cc504231354aaf00624246d9b2c76
Robin Tibor Schirrmeister <robintibor@gmail.com>
2015-04-08 19:37:57 +0200

    cudamat conv and some rfactorings

--- convolutions.py
+++ convolutions.py
@@ -3,17 +3,24 @@ import theano
 import pycuda.driver as cuda
 from pycuda.compiler import SourceModule
 import theano.tensor.nnet.conv3d2d
+import cudamat
 conv_mod = SourceModule(open('conv.cu').read())
 loop_conv_on_gpu_func = conv_mod.get_function("loop_conv")
+from timeit import default_timer as timer
+import theano.tensor as T
 
+def compute_out_shape(inputs_shape, filters_shape):
+    num_batches = inputs_shape[0]
+    out_height = inputs_shape[1] - filters_shape[1] + 1;
+    out_width = inputs_shape[2] - filters_shape[2] + 1;
+    out_duration = inputs_shape[3] - filters_shape[3] + 1;
+    num_filters = filters_shape[0]
+    return (num_batches, out_height, out_width, out_duration, 
+        num_filters)
+    
 def loop_conv_on_gpu(inputs, filters, bias):
-    num_batches = inputs.shape[0]
-    out_height = inputs.shape[1] - filters.shape[1] + 1;
-    out_width = inputs.shape[2] - filters.shape[2] + 1;
-    out_duration = inputs.shape[3] - filters.shape[3] + 1;
-    num_filters = filters.shape[0]
-    output = np.zeros((num_batches, out_height, out_width, out_duration, 
-        num_filters)).astype(np.float32)
+    out_shape = compute_out_shape(inputs.shape, filters.shape)
+    output = np.zeros(out_shape).astype(np.float32)
     loop_conv_on_gpu_func(cuda.In(inputs), cuda.In(filters),
         cuda.In(bias), cuda.InOut(output),
         np.int32(inputs.shape[0]),np.int32(inputs.shape[1]),
@@ -60,13 +67,14 @@ def loop_conv(X, W, b):
     input_duration = X.shape[3]
     input_channels = X.shape[4]
     assert(filt_channels == input_channels)
+    out_shape = compute_out_shape(X.shape, W.shape)
     
-    out_height = input_height - filt_height + 1
-    out_width = input_width - filt_width + 1
-    out_duration = input_duration - filt_duration + 1
+    out_height = out_shape[1]
+    out_width = out_shape[2]
+    out_duration = out_shape[3]
     
     # The output is H :)
-    H = np.zeros((num_batches,out_height,out_width,out_duration,num_filters))
+    H = np.zeros((out_shape))
     for batch_i in xrange(0, num_batches):
         for out_x in xrange(0, out_height):
             for out_y in xrange(0, out_width):
@@ -110,7 +118,8 @@ def vectorized_conv(X, W, b):
     # Collect all flattened input into matrix 
     # #filterpoints x #batches*#outputpoints
     flat_out_length = out_height * out_width * out_duration # for single filter/output chan
-    inputs_flat = np.zeros((filter_vec_length, num_batches * flat_out_length))
+    inputs_flat = np.zeros((filter_vec_length, num_batches * flat_out_length),
+        dtype=np.float32)
     for out_x in xrange(0, out_height):
         for out_y in xrange(0, out_width):
             for out_z in xrange(0, out_duration):
@@ -162,6 +171,174 @@ def vectorized_conv(X, W, b):
     result_mat = result_mat.T + b
     H = np.reshape(result_mat,  (num_batches, out_height, out_width, out_duration, num_filters))
     return H
+
+def vectorized_cudamat_conv(X, W, b):
+    num_filters = W.shape[0]
+    filt_height = W.shape[1]
+    filt_width = W.shape[2]
+    filt_duration = W.shape[3]
+    filt_channels = W.shape[4]
+    num_batches = X.shape[0]
+    in_height = X.shape[1]
+    in_width = X.shape[2]
+    in_duration = X.shape[3]
+    in_channels = X.shape[4]
+    # We assume filter always convolves directly over all channels =>
+    # has size of input channels in channel dimension
+    assert(filt_channels == in_channels)
+    out_height = in_height - filt_height + 1
+    out_width = in_width - filt_width + 1
+    out_duration = in_duration - filt_duration + 1
+    
+    # Flatten filters into matrix #filters x #filterpoints
+    filter_vec_length = filt_height * filt_width * filt_duration * \
+        filt_channels
+    filter_mat = np.reshape(W, (num_filters, filter_vec_length))
+
+    # Collect all flattened input into matrix 
+    # #filterpoints x #batches*#outputpoints
+    flat_out_length = out_height * out_width * out_duration # for single filter/output chan
+    inputs_flat = np.zeros((filter_vec_length, num_batches * flat_out_length),
+        dtype=np.float32)
+    
+    for out_x in xrange(0, out_height):
+        for out_y in xrange(0, out_width):
+            for out_z in xrange(0, out_duration):
+                # Take input points corresponding
+                # to the part of input covered by filters for 
+                # given output point(outx, outy, outz)
+                # (always take all chans)
+                input_part = X[:, out_x:out_x+filt_height, out_y:out_y+filt_width,
+                    out_z:out_z+filt_duration, :]
+                # Flatten the patches of input
+                input_part_flat = \
+                    input_part.reshape((num_batches, filter_vec_length)).T
+                
+                # Now we have columns of flattened input, 
+                # each column corresponding to one batch,
+                # e.g.for 2 batches and flat filter length of 3
+                # column 1 is for input 1 and column 2 is for input 2
+                # in1_1 in2_1
+                # in1_2 in2_2
+                # in1_3 in2_3
+                # For easier reshaping we distribute these columns in
+                # the final matrix so that all columns for one batch will be
+                # adjacent, i.e. in the final matrix, we will have columns:
+                # in1 in1 in1 in2 in2 in2
+                # instead of:
+                # in1 in2 in1 in2 in1 in2
+                # flattened out index
+                out_index = out_x * out_width * out_duration + out_y * out_duration + out_z
+                # Compute inds for that particular output point spaced over whole
+                # flattened matrix columns
+                # e.g. we can get
+                # 0,3 as indices if we have 2 batches and 3 points
+                # x - - x - - 
+                # and next iteration we get
+                # - x - - x - so that all columns of one batch are adjacent
+                # as explained above
+                inds = np.arange(out_index, 
+                    out_index + (num_batches * flat_out_length),
+                    step=flat_out_length)
+                inputs_flat[:, inds.astype(int)] = input_part_flat
+    # 60 ms for loop 30 ms for dot. ..dominated by loop 
+    result_mat = cudamat.dot(cudamat.CUDAMatrix(filter_mat), 
+        cudamat.CUDAMatrix(inputs_flat)).asarray()
+    # Add bias
+    # Also, for the reshape afterwards we first transpose the matrix 
+    # (which is #filters x #flatoutputpoints)
+    # so that filters are the second dimension and change fastest =>
+    # correct H result matrix (note that num_filters is last dimension, not first)
+    result_mat = result_mat.T + b
+    H = np.reshape(result_mat,  (num_batches, out_height, out_width, out_duration, num_filters))
+    return H
+
+input_a = T.fmatrix()
+input_b = T.fmatrix()
+mul_theano_result = T.dot(input_a,input_b)
+mul_theano_function = theano.function([input_a,input_b], 
+    mul_theano_result)
+
+def vectorized_theano_conv(X, W, b):
+    num_filters = W.shape[0]
+    filt_height = W.shape[1]
+    filt_width = W.shape[2]
+    filt_duration = W.shape[3]
+    filt_channels = W.shape[4]
+    num_batches = X.shape[0]
+    in_height = X.shape[1]
+    in_width = X.shape[2]
+    in_duration = X.shape[3]
+    in_channels = X.shape[4]
+    # We assume filter always convolves directly over all channels =>
+    # has size of input channels in channel dimension
+    assert(filt_channels == in_channels)
+    out_height = in_height - filt_height + 1
+    out_width = in_width - filt_width + 1
+    out_duration = in_duration - filt_duration + 1
+    
+    # Flatten filters into matrix #filters x #filterpoints
+    filter_vec_length = filt_height * filt_width * filt_duration * \
+        filt_channels
+    filter_mat = np.reshape(W, (num_filters, filter_vec_length))
+
+    # Collect all flattened input into matrix 
+    # #filterpoints x #batches*#outputpoints
+    flat_out_length = out_height * out_width * out_duration # for single filter/output chan
+    inputs_flat = np.zeros((filter_vec_length, num_batches * flat_out_length),
+        dtype=np.float32)
+    
+    for out_x in xrange(0, out_height):
+        for out_y in xrange(0, out_width):
+            for out_z in xrange(0, out_duration):
+                # Take input points corresponding
+                # to the part of input covered by filters for 
+                # given output point(outx, outy, outz)
+                # (always take all chans)
+                input_part = X[:, out_x:out_x+filt_height, out_y:out_y+filt_width,
+                    out_z:out_z+filt_duration, :]
+                # Flatten the patches of input
+                input_part_flat = \
+                    input_part.reshape((num_batches, filter_vec_length)).T
+                
+                # Now we have columns of flattened input, 
+                # each column corresponding to one batch,
+                # e.g.for 2 batches and flat filter length of 3
+                # column 1 is for input 1 and column 2 is for input 2
+                # in1_1 in2_1
+                # in1_2 in2_2
+                # in1_3 in2_3
+                # For easier reshaping we distribute these columns in
+                # the final matrix so that all columns for one batch will be
+                # adjacent, i.e. in the final matrix, we will have columns:
+                # in1 in1 in1 in2 in2 in2
+                # instead of:
+                # in1 in2 in1 in2 in1 in2
+                # flattened out index
+                out_index = out_x * out_width * out_duration + out_y * out_duration + out_z
+                # Compute inds for that particular output point spaced over whole
+                # flattened matrix columns
+                # e.g. we can get
+                # 0,3 as indices if we have 2 batches and 3 points
+                # x - - x - - 
+                # and next iteration we get
+                # - x - - x - so that all columns of one batch are adjacent
+                # as explained above
+                inds = np.arange(out_index, 
+                    out_index + (num_batches * flat_out_length),
+                    step=flat_out_length)
+                inputs_flat[:, inds.astype(int)] = input_part_flat
+    # 70 ms for loop 30 ms for dot. ..dominated by loop 
+    result_mat = mul_theano_function(filter_mat, inputs_flat)
+    # Add bias
+    # Also, for the reshape afterwards we first transpose the matrix 
+    # (which is #filters x #flatoutputpoints)
+    # so that filters are the second dimension and change fastest =>
+    # correct H result matrix (note that num_filters is last dimension, not first)
+    result_mat = result_mat.T + b
+    H = np.reshape(result_mat,  (num_batches, out_height, out_width, out_duration, num_filters))
+    return H
+
 
 def loop_conv_flat(X, W, b):
     # Go over all five dimensions 
--- perf_convs.py
+++ perf_convs.py
@@ -1,19 +1,29 @@
+#!/usr/bin/env python
+
 import theano
 # also see http://deeplearning.net/software/theano/tutorial/gpu_data_convert.html 
 # for pycuda /theano interplay
 import theano.misc.pycuda_init
 from test_data import generate_theano_test_data
 from convolutions import loop_conv_on_gpu, create_theano_conv3d, \
-    vectorized_conv, loop_conv, create_theano_conv3d2d
+    vectorized_conv, loop_conv, create_theano_conv3d2d, compute_out_shape, \
+    vectorized_cudamat_conv, vectorized_theano_conv
 
 from timeit import default_timer as timer
 import numpy as np
 import argparse
+import cudamat
+import re
+# set to same gpu as theano
+cudamat.cuda_set_device(int(re.search("gpu([0-4])", theano.config.device).group(1)))
+cudamat.init()
+
 
 def perffunc(name, func, correct_result, *func_args):
     total_running_time = 0
     runs = 0
-    while total_running_time < 1:
+    while (total_running_time < 1 or 
+        (total_running_time < 2 and runs < 30)):
         start = timer()
         if (len(func_args) > 0):
             result = func(*func_args)
@@ -25,18 +35,22 @@ def perffunc(name, func, correct_result, *func_args):
     print("{:25s} runtime per iteration ({:4d} iterations): {:8.4f}ms".format(
         name, runs, 1000 * total_running_time / runs))
     diff = np.sum(np.square(correct_result - result))
-    assert diff < 1e-5
+    assert diff < 1e-5, "Diff {:f} too big for {:s}".format(diff, name)
 
 def perf_3d_convs(inputs_shape, filters_shape):
-    #inputs_shape=(15, 20, 16, 15, 3)
-    #filters_shape=(7, 3, 6, 2, 3)
     inputs, filters, bias = generate_theano_test_data(np.random,inputs_shape,
         filters_shape)
+    output_shape = compute_out_shape(inputs.eval().shape, filters.eval().shape)
+    
     print("Batches/Filters, rows, columns, times, channels")
     print("Input shape  {:s}".format(inputs_shape))
     print("Filter shape {:s}".format(filters_shape))
+    print("Output shape {:s}".format(output_shape))
     print("#Inputs  {:7d}".format(np.prod(inputs_shape)))
     print("#Weights {:7d}".format(np.prod(filters_shape)))
+    print("#Outputs {:7d}".format(np.prod(output_shape)))
+    print("#Multiplications {:7d}".format(
+        np.prod(filters_shape) * np.prod(output_shape)))
     theano_3d_func = create_theano_conv3d(inputs, filters, bias)
     # flipping from https://groups.google.com/forum/#!msg/theano-users/1S9_bZgHxVw/0cQR9a4riFUJ
     filters_flip = filters[:,::-1,::-1,::-1,:]  # flip width, height and time
@@ -47,12 +61,18 @@ def perf_3d_convs(inputs_shape, filters_shape):
     correct_result = theano_3d_func()
     perffunc("Theano 3d", theano_3d_func, correct_result)
     perffunc("Theano 3d2d", theano_3d_2d, correct_result)
-    perffunc("GPU Loop Conv", loop_conv_on_gpu, correct_result, 
+    perffunc("Python Vectorized", vectorized_conv, correct_result, 
         inputs_val, filters_val, bias_val)
-    perffunc("Python Vectorized Conv", vectorized_conv, correct_result, 
+    perffunc("Cudamat Vectorized", vectorized_cudamat_conv, correct_result, 
         inputs_val, filters_val, bias_val)
-    perffunc("Python Loop Conv", loop_conv, correct_result, 
+    perffunc("Theano Mul Vectorized", vectorized_theano_conv, correct_result, 
         inputs_val, filters_val, bias_val)
+    perffunc("GPU Loop", loop_conv_on_gpu, correct_result, 
+        inputs_val, filters_val, bias_val)
+    perffunc("Python Loop", loop_conv, correct_result, 
+        inputs_val, filters_val, bias_val)
+    
+
     
 def parse_command_line_arguments():
     parser = argparse.ArgumentParser(
@@ -71,4 +91,13 @@ def parse_command_line_arguments():
 
 if __name__ == "__main__":
     args = parse_command_line_arguments()
+    inputs, filters, bias = generate_theano_test_data(np.random,args.inputs,
+        args.filters)
     perf_3d_convs(args.inputs, args.filters)
+    # python perf_convs.py --inputs 20 20 20 20 3 --filters 7 3 6 2 3
+    # python perf_convs.py --inputs 6 5 8 2 3 --filters 4 3 6 1 3
+    # fails on theano3d2d for some reason: ? 
+    #(notice second last dimension same size)
+    # python perf_convs.py --inputs 6 5 8 2 3 --filters 4 3 6 2 3
+
+    #cudamat.shutdown()
