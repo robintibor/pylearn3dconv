51e41dd059523db49ab70e75eb140fa4a4c1ace7
Robin Tibor Schirrmeister <robintibor@gmail.com>
2015-04-18 18:16:29 +0200

    slightly faster pooling with cleane code

--- pylearn3dconv/theanodnn/pool.py
+++ pylearn3dconv/theanodnn/pool.py
@@ -1,5 +1,6 @@
 import theano.tensor as T
 from theano.sandbox.cuda.dnn import dnn_pool
+from theano.sandbox.cuda.basic_ops import gpu_alloc_empty
 
 def dnn_pool3d2d(inputs, pool_shape, pool_stride, image_shape, mode='max'):
     """ Pool first all time-slices, so 2d-poolings over width and height.
@@ -10,25 +11,29 @@ def dnn_pool3d2d(inputs, pool_shape, pool_stride, image_shape, mode='max'):
             " or equal than image shape, {:d} > {:d} for "
             "pool_shape: {:s}, image_shape:{:s}").format(pool_shape[i],
                 image_shape[i], pool_shape, image_shape)
-    first_2d_pooled_outputs = []
+    output_shape = [((image_shape[i] - pool_shape[i]) // pool_stride[i]) + 1 
+        for i in xrange(3)]
+    output2d_pooled = gpu_alloc_empty(inputs.shape[0], inputs.shape[1],
+        output_shape[0], output_shape[1], image_shape[2])
     for z in range(image_shape[2]):
         pooled_slice = dnn_pool(inputs[:,:,:,:,z], ws=pool_shape[0:2], 
             stride=pool_stride[0:2], mode=mode)
-        first_2d_pooled_outputs.append(pooled_slice)
+        output2d_pooled = T.set_subtensor(output2d_pooled[:,:,:,:,z], pooled_slice)
     
-    first_2d_pooled_output = T.stack(first_2d_pooled_outputs)[0,:,:,:,:,:]
-    first_2d_pooled_output = first_2d_pooled_output.dimshuffle(1,2,3,4,0)
+    
     # now 1d-pool over last dimension...
     # could use first or second dimension as input of pool1d..
     # compute maximum y index after first pooling
-    max_y = ((image_shape[1] - pool_shape[1]) // pool_stride[1]) + 1
-    final_outputs = []
+    output = gpu_alloc_empty(inputs.shape[0], inputs.shape[1],
+        output_shape[0], output_shape[1], output_shape[2])
+    max_y = output_shape[1]
     for y in range(max_y):
-        final_pooled_slice = dnn_pool(first_2d_pooled_output[:,:,:,y,:], 
+        # ignore first=0 dimension, alrdy pooled in loop before
+        # so set stride and shape to 1 there
+        final_pooled_slice = dnn_pool(output2d_pooled[:,:,:,y,:], 
             ws=(1, pool_shape[2]), 
             stride=(1, pool_stride[2]), mode=mode)
-        final_outputs.append(final_pooled_slice)
+        output = T.set_subtensor(output[:,:,:,y,:], final_pooled_slice)
+
+    return output
     
-    final_output = T.stack(final_outputs)[0,:,:,:,:,:]     
-    final_output = final_output.dimshuffle(1,2,3,0,4)
-    return final_output
